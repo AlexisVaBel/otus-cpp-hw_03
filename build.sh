0151b6412b1ea297857a7bd0210d0472945817ad
AlexisVaBel <AlexisVaBel@gmail.com>
2019-07-01 16:29:12 +0700

    sh updated

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ cd ./build
 cmake ..
 make -j4
 #./allocator
+ltrace -e malloc -e free ./allocator
 #ltrace -e malloc -e free ./allocator > /dev/null
-#valgrind --tool=memcheck ./allocator 
-valgrind --leak-check=full -v  ./allocator
+#valgrind  ./allocator 
+#valgrind --tool=memcheck  ./allocator 
+#valgrind --leak-check=full -v  ./allocator
