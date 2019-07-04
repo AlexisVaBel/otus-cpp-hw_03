f0013d0d3df3fd0320c07ce3eb58cb6e70faee7c
AlexisVaBel <AlexisVaBel@gmail.com>
2019-07-05 02:35:10 +0700

    Fixed allocator

--- alloc/allocatorpull.h
+++ alloc/allocatorpull.h
@@ -15,6 +15,11 @@ public:
     using size_type           = std::size_t;
 public:
 
+    AllocatorPull():m_szLimit(0),m_szCurStep(0),m_pnt(nullptr),m_pntCur(nullptr){}
+    ~AllocatorPull(){
+        if(m_pnt != nullptr) free(m_pnt);
+    }
+
     template<typename U>
     struct rebind {
         typedef AllocatorPull<U,CNT_RESERVE> other;
@@ -22,31 +27,28 @@ public:
 
     pointer allocate(std::size_t n)
     {
-        if(m_pnt == nullptr || m_szCurStep >= m_szLimit){
-            m_szLimit = n * sizeof (value_type) * CNT_RESERVE + sizeof (value_type);
-            m_szCurStep = 0;
-            auto p = std::malloc(m_szLimit);
+        if(m_pnt == nullptr){
+            auto p = std::malloc(n * sizeof (value_type)* CNT_RESERVE);
             if (!p)
                 throw std::bad_alloc();
             m_pnt = reinterpret_cast<pointer>(p);
-//            std::cout << __PRETTY_FUNCTION__ << " arranged "<<m_szLimit <<std::endl;
-        }else{
-            m_szCurStep+= n * sizeof (value_type);
+            m_pntCur = m_pnt;
         }
-//        std::cout << __PRETTY_FUNCTION__ << " "<<m_szCurStep<<std::endl;
-        return reinterpret_cast<pointer> (m_pnt + m_szCurStep);
-
+        return reinterpret_cast<pointer> (m_pntCur++);
     }
 
     void deallocate(T *p, std::size_t n)
     {
+
+
 //        std::cout << __PRETTY_FUNCTION__ << " "<<m_szCurStep<<std::endl;
-        if(m_szCurStep <= 0) {
-            delete m_pnt;
+        if(m_pntCur == m_pnt) {
+            free(m_pnt);
             m_pnt = nullptr;
         }
         else
-            m_szCurStep -= n * sizeof (T);
+            m_pntCur--;
+
 
     }
 
@@ -71,6 +73,7 @@ private:
     size_type m_szCurStep;
 
     pointer m_pnt;
+    pointer m_pntCur;
 };
 
 #endif // ALLOCATORPULL_H
--- allocator.cpp
+++ allocator.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+#include <vector>
 #include <map>
 #include <container/clist.h>
 
 #include <alloc/allocatorpull.h>
 
 static int const I_ELM_CNT = 9;
+static int const I_ELM_ALLOC = 10;
 
 // some functional works ))
 template <int ival>
@@ -18,7 +20,9 @@ struct Factorial<0>{
     static const int ifact = 1;
 };
 
-template<int ival,class T, class ... Args>
+
+
+template<int ival, class T, class ... Args>
 struct Cyclce_Map{
     static void produceFact(std::map<T,Args ...> &map){
         map[ival]=Factorial<ival>::ifact;
@@ -48,21 +52,22 @@ void print_lst_value(CList<T,Args ...> lst){
 }
 
 int main(int, char *[]) {
-    std::map <int, int> std_alloc_map;
-    std::map <int, int,std::less<int>, AllocatorPull<std::pair<const int, int>,I_ELM_CNT>> cust_alloc_map;
+//    std::map <int, int> std_alloc_map;
+//    std::map <int, int,std::less<int>, AllocatorPull<std::pair<const int, int>,I_ELM_ALLOC>> cust_alloc_map;
 
-    Cyclce_Map<I_ELM_CNT,int,int>::produceFact(std_alloc_map);
-    Cyclce_Map<I_ELM_CNT,int,int,std::less<int>, AllocatorPull<std::pair<const int, int>,I_ELM_CNT>>::produceFact(cust_alloc_map);
+//    Cyclce_Map<I_ELM_CNT,int,int>::produceFact(std_alloc_map);
+//    Cyclce_Map<I_ELM_CNT,int,int,std::less<int>, AllocatorPull<std::pair<const int, int>,I_ELM_ALLOC>>::produceFact(cust_alloc_map);
 
 
 
-    for(auto k: std_alloc_map){
-        std::cout << k.first << " " << k.second << std::endl;
-    }
+//    for(auto k: std_alloc_map){
+//        std::cout << k.first << " " << k.second << std::endl;
+//    }
 
-    for(auto k: cust_alloc_map){
-        std::cout << k.first << " " << k.second << std::endl;
-    }
+//    for(auto k: cust_alloc_map){
+//        std::cout << k.first << " " << k.second << std::endl;
+//    }
+
 
 
     CList<int,std::allocator<int>> std_alloc_lst;
@@ -74,16 +79,16 @@ int main(int, char *[]) {
 
 
 
-    auto itStdalloc = std_alloc_lst.begin();
-    while(itStdalloc != std_alloc_lst.end()){
-        std::cout <<(*itStdalloc).value << std::endl;
-        ++itStdalloc;
-    }
-    itStdalloc = cust_alloc_lst.begin();
-    while(itStdalloc != std_alloc_lst.end()){
-        std::cout <<(*itStdalloc).value << std::endl;
-        ++itStdalloc;
-    }
+//    auto itStdalloc = std_alloc_lst.begin();
+//    while(itStdalloc != std_alloc_lst.end()){
+//        std::cout <<(*itStdalloc).value << std::endl;
+//        ++itStdalloc;
+//    }
+//    itStdalloc = cust_alloc_lst.begin();
+//    while(itStdalloc != std_alloc_lst.end()){
+//        std::cout <<(*itStdalloc).value << std::endl;
+//        ++itStdalloc;
+//    }
 
     return 0;
 }
